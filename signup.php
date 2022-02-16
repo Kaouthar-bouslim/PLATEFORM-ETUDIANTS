6a62b2f0227ff37047cf48be35794870f5808e7a
Kaouthar-bouslim <78320053+Kaouthar-bouslim@users.noreply.github.com>
2022-02-16 21:30:11 +0100

    Update signup.php

--- signup.php
+++ signup.php
@@ -6,14 +6,23 @@ $user=$_POST['user'];
 $email=$_POST['email'];
 $pass=$_POST['passwd'];
 
+$sql = "SELECT nom FROM user WHERE nom ='".addslashes($user)."' ";
+
+
+$res = mysqli_query($mysqli, $sql);
+$rowcount=mysqli_num_rows($res);
+if($rowcount==1){
+  header("Location:signup1.php?error=Error"); 
+
+}else{
+
+
 $req="INSERT into user(nom,email,passwrd) values ('$user','$email','$pass')";
 $rs=mysqli_query($mysqli,$req) or die (mysqli_error($mysqli));
 
 if($rs){
     header("location:confirm.html");
 }
-else{
-  header("Location:signup.php?error=Error"); 
 
 }
 ?> 

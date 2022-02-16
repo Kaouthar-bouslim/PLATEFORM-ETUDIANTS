81f8813e5378f1a7e3ed7cd5686a8ef5ccfb4f07
Kaouthar-bouslim <78320053+Kaouthar-bouslim@users.noreply.github.com>
2022-02-16 22:45:45 +0100

    Add files via upload

---
+++ GINF.php
@@ -0,0 +1,63 @@
+<?php 
+  require_once "verif1.php";
+
+  require_once "head.html";
+
+?>
+
+
+<?php 
+  require_once "verif1.php";
+
+  require_once "head.html";
+  
+?>
+
+<style>
+  .texto{
+   padding-top: 7%;
+  } 
+   body{
+     background-color: darkcyan;
+   }
+   a{
+     color: cyan;
+   }
+   p{
+       width: 60%;
+   }
+   img{
+       width: 60%;
+       padding-bottom: 10px;
+   }
+
+</style>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+
+  <div class=texto>
+   <center>
+   <h1>GENIE INFORMATIQUE :</h1>
+
+       <p style="color: white;">La filière Génie Informatique: cette filière forme des ingénieurs capables d’intervenir dans l’étude de projets, le développement de logiciels, la maîtrise de la conception, de la programmation et du test des logiciels, la gestion de centres informatiques, l’administration de réseaux et de base de données.
+
+</p>
+     <img src="http://ensat.ac.ma/Portail/wp-content/uploads/2019/02/GINF-S1.jpg" alt="" srcset="">
+     <img src="http://ensat.ac.ma/Portail/wp-content/uploads/2019/02/GINF-S2.jpg" alt="" srcset="">
+     <img src="http://ensat.ac.ma/Portail/wp-content/uploads/2019/02/GINF-S3.jpg" alt="" srcset="">
+     <img src="http://ensat.ac.ma/Portail/wp-content/uploads/2019/02/GINF-S4.jpg" alt="" srcset="">
+     <img src="http://ensat.ac.ma/Portail/wp-content/uploads/2019/02/GINF-S5.jpg" alt="" srcset="">
+     <img src="http://ensat.ac.ma/Portail/wp-content/uploads/2019/02/GINF-S5-.jpg" alt="" srcset="">
+
+
+  </center>
+</div>
+</body>
+</html>

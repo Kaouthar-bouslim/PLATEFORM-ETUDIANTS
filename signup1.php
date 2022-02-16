cba974fff0a9a0327580992294e11442e42167e8
Kaouthar-bouslim <78320053+Kaouthar-bouslim@users.noreply.github.com>
2022-02-16 21:31:09 +0100

    Update signup1.php

--- signup1.php
+++ signup1.php
@@ -8,10 +8,13 @@
 	<link rel="icon" type="image/png" href="images/favicon.ico"/>
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style2.css">
 
 	<link rel="stylesheet" type="text/css" href="css/util.css">
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 	<script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+	<script src="https://kit.fontawesome.com/c4254e24a8.js" crossorigin="anonymous"></script>
+
 
 <!--===============================================================================================-->
 </head>
@@ -34,25 +37,38 @@
                      {
                         if($_GET['error'] == "Error")
                         {
-                            echo '<br><center><br><h5 >Votre nom d\'utilisateur ou votre Mot de passe est Incorrect</h5><center>';
-                        
+							echo '<br><center><br><p style="color:red"  >This Username already exist. </p><center>';
+
                         }
                        
                      }
 					 ?>
-					 
+						<div class="input-group">
+
 					<div class="wrap-input100 validate-input m-t-85 m-b-35" data-validate = "Enter username">
-						<input class="input100" type="text" name="user" placeholder="Username" required>
-					</div>
+						<input class="input100" type="text" name="user" placeholder="Username" id="contact-name" onkeyup="validateName()" required>
+						<span id="name-error"></span>
 
+					</div>
+					</div>
+						<div class="input-group">
                     <div class="wrap-input100 validate-input m-b-50" data-validate="Enter password">
-						<input class="input100" type="password" name="passwd" placeholder="Password" required>
+						<input class="input100" type="password" name="passwd" placeholder="Password" id="contact-password" onkeyup="validatePassword()" required>
+						<span id="password-error"></span>
+
+					</div>
+					
 					</div>
 
-					<div class="wrap-input100 validate-input m-b-50" data-validate="Enter Email">
-						<input class="input100" type="text" name="email" placeholder="Email" required>
+					<div class="input-group">
+					<div class="wrap-input100 validate-input m-b-50" data-validate="Enter Email" >
+						<input class="input100" type="text" name="email" placeholder="Email" id="contact-email" onkeyup="validateEmail()" required>
+						<span id="email-error"></span>
+
 					</div>
-				
+
+					</div>
+					
 					<div class="container-login100-form-btn">
 						<input class="btn btn-info" type="submit" name="entrer" value="Sign Up">
 							
@@ -76,7 +92,7 @@
 		</div>
 	</div>
 	
-
+	<script src="css/script.js"></script>
 
 
 </body>
